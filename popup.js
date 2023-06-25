0236113c8b62ac2b65b8719bf16481a276ca783b
hiresh <hiresh.at@gmail.com>
2023-06-25 12:43:43 +0530

    fix footer and initial popup size

--- popup.js
+++ popup.js
@@ -23,26 +23,32 @@ document.addEventListener("DOMContentLoaded", function () {
       delayTimeInput2.value = 1;
     }
   });
-  function generateSquares() {
-    const squareCountInput = document.getElementById("squareCount");
-    const squareCount = parseInt(squareCountInput.value, 10);
+  const squareCountInput = document.getElementById("squareCount");
+    let squareCount = parseInt(squareCountInput.value, 10);
 
-    const delayTimeInput = document.getElementById("delayTime");
-    const delayTime = parseFloat(delayTimeInput.value, 5) * 1000; // Convert to milliseconds
+    let delayTimeInput = document.getElementById("delayTime");
+    let delayTime = parseFloat(delayTimeInput.value, 5) * 1000; // Convert to milliseconds
     
     squaresContainer.innerHTML = ""; // Clear existing squares
 
-    const squares = [];
+    let squares = [];
     let currentIndex = 0;
     let currentBreath = 0;
+  function generateSquares() {
+    generateBtn.disabled = true;
+    generateBtn.style.backgroundColor = 'grey';
+    squaresContainer.innerHTML = ""; // Clear existing squares
+     squares = [];
+     currentIndex = 0;
+     currentBreath = 0;
+      squareCount = parseInt(squareCountInput.value, 10);
 
-    function createSquare() {
-      const square = document.createElement("div");
-      square.classList.add("square");
-      squaresContainer.appendChild(square);
-      squares.push(square);
+      delayTimeInput = document.getElementById("delayTime");
+      delayTime = parseFloat(delayTimeInput.value, 5) * 1000; // Convert to milliseconds
+    for (let i = 0; i < 5; i++) {
+      console.log(squareCount);
+      createSquare();
     }
-
     function showNextSquare() {
       if (currentIndex < squares.length) {
         
@@ -70,7 +76,8 @@ document.addEventListener("DOMContentLoaded", function () {
           setTimeout(showConfetti, 3000);
           setTimeout(showConfetti, 5000);
           setTimeout(showConfetti, 7000);
-          
+          generateBtn.disabled = false;
+          generateBtn.style.backgroundColor = 'green';
          
         }
       }
@@ -79,23 +86,30 @@ document.addEventListener("DOMContentLoaded", function () {
     function randomInRange(min, max) {
       return Math.random() * (max - min) + min;
     }
+   
 
     function showConfetti(){
       confetti({
         angle: randomInRange(55, 125),
         spread: randomInRange(50, 70),
         particleCount: randomInRange(50, 100),
-        origin: { y: 0.6 },
+        origin: { y: 0 },
       });
     }
 
-    for (let i = 0; i < 5; i++) {
-      console.log(squareCount);
-      createSquare();
-    }
+   
 
     setTimeout(showNextSquare, 2000);
   }
-
+  function createSquare() {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    squaresContainer.appendChild(square);
+    squares.push(square);
+  }
+  for (let i = 0; i < 5; i++) {
+    console.log(squareCount);
+    createSquare();
+  }
   generateBtn.addEventListener("click", generateSquares);
 });
